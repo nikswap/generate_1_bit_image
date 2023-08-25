dc37aa01189d98b1a1e4bb32226acafebb40adf0
Nik Swap <nikswap@local.host>
2023-08-25 23:15:21 +0200

    Updated code with ~ operator

--- generate_funk.py
+++ generate_funk.py
@@ -1,6 +1,6 @@
 import random
 
-operators = ['*','//','+','-']
+operators = ['*','//','+','-','~']
 vars = ['x','y']
 
 def get_a_funk():
@@ -11,11 +11,27 @@ def get_a_funk():
         op = random.choice(operators)
         var1 = random.choice(vars)
         var2 = random.choice(vars)
-        res.append(op.join([var1,var2]))
-    for i in range(len(res)-1):
+        if op in '~':
+            t = var1
+            var1 = random.choice(operators)
+            while var1 in '~':
+                var1 = random.choice(operators)
+            res.append(t+var1+op+var2)
+        else:
+            res.append(op.join([var1,var2]))
+    #print(res)
+    for i in range(0,len(res)-1,2):
         op = random.choice(operators)
         op2 = random.choice(operators)
-        res2 += res[i]+op+res[i+1]+op2
+        #Lazy fix. This should be able to ~ as well
+        while op2 in '~':
+            op2 = random.choice(operators)
+        #print(op,op2,op in '~')
+        if op in '~':
+            res2 += res[i]+op2+op+res[i+1]+op2
+        else:
+            res2 += res[i]+op+res[i+1]+op2
+        
     if res2[-1] == '/':
         res2 = res2[:-1]
     return res2[:-1]
